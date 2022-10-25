fa38b79b02ae4190162c9416b3be025e28eb3a30
unknown <trongphuc5667@gmail.com>
2022-10-25 11:54:21 +0700

    fix

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
 
 app.use(
   cors({
-    origin: "https://fooddelivery-livid.vercel.app/",
+    origin: "https://fooddelivery-livid.vercel.app",
     // origin: "http://localhost:3000",
     credentials: true,
   })
